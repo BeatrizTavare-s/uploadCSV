a7b1ea8894d7d69b922d039f90fa84bc106535aa
Beatriz Tavares <beatriz98t@gmail.com>
2024-05-13 16:58:03 -0300

    feat: create component ModalEdit

---
+++ src/app/components/ModalEdit.tsx
@@ -0,0 +1,48 @@
+"use client"
+import { Button, Input, Modal} from 'antd';
+import { useState } from 'react'; 
+import { Item } from "@/app/components/ListItems";
+
+
+interface ModalEditItem {
+    selectedItem: Item,
+    onCloseModal: () => void,
+    visible: boolean,
+    onSaveEdit: (itemEdit: Item) => void,
+}
+
+export const ModalEdit = ({selectedItem, onCloseModal, visible, onSaveEdit}:ModalEditItem) =>{
+    const [itemEdit, setItemEdit] = useState(selectedItem)
+
+
+    const handleCloseModal = () => {
+        onCloseModal()
+    };
+
+    const handleSavEdit = () => {
+        console.log("valor editado selected",itemEdit)
+        onSaveEdit(itemEdit)
+        onCloseModal()
+    };
+ 
+    return(
+        <Modal
+        key={itemEdit?.id}
+        open={visible}
+        title={"Detalhes Item código: "+itemEdit.code}
+        onCancel={handleCloseModal}
+        footer={[
+            <Button key="back" onClick={handleCloseModal}>
+               Fechar
+            </Button>,
+            <Button key="save" onClick={handleSavEdit}>
+                Ok
+            </Button>,
+        ]} 
+        >
+            <Input  title='Descrição' placeholder="Descrição" key="descriptionModal" value={itemEdit?.description} onChange={(e) => setItemEdit({...itemEdit, description: e.target.value})} />
+            <Input  title='Quantidade' placeholder="Quantidade"  key="quantityModal" value={itemEdit?.quantity} onChange={(e) => setItemEdit({...itemEdit, quantity: Number(e.target.value)})}/>
+            <Input  title='Preço' placeholder="Preço" key="priceModal" value={itemEdit?.price} onChange={(e) => setItemEdit({...itemEdit, price: Number(e.target.value)})} />
+    </Modal>
+    )
+}
