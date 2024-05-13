d216863fc019079d611f6632690e2af125072853
Beatriz Tavares <beatriz98t@gmail.com>
2024-05-13 16:57:24 -0300

    feat: create component uploadFile

---
+++ src/app/components/UploadFile.tsx
@@ -0,0 +1,33 @@
+import { Button, Upload, UploadProps, message } from "antd";
+import { UploadOutlined } from '@ant-design/icons';
+import React from "react";
+
+interface FuncticionFetchItems {
+  onFechItems(): void;
+}
+
+const UpdateFile: React.FC<FuncticionFetchItems> = ({onFechItems}) => {
+  const props: UploadProps = {
+    name: 'file',
+    action: `${process.env.NEXT_PUBLIC_URL_API}/api/upload`,
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+        onFechItems()
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
+  return (
+    <Upload {...props}>
+    <Button icon={<UploadOutlined />}>Click to Upload</Button>
+  </Upload>
+  );
+};
+
+export default UpdateFile;
