b2d97bd33286c7bc97afab1348a76d9632efcd66
Beatriz Tavares <beatriz98t@gmail.com>
2024-05-15 20:19:01 -0300

    feat: add try catch api route

--- src/app/api/item/route.ts
+++ src/app/api/item/route.ts
@@ -2,15 +2,15 @@
 import prisma from '../../lib/prisma'
 import toObject from '../../utils/toObject'
 import { NextRequest } from 'next/server';
+import { Prisma } from '@prisma/client';
 
 
-
-async function getItemById(id:string){
+async function getItemById(id: string) {
     const item = await prisma.item.findUnique({
-        where:{
+        where: {
             id: Number(id)
         },
-        select:{
+        select: {
             id: true,
             code: true,
             description: true,
@@ -25,74 +25,94 @@ async function getItemById(id:string){
     return item
 }
 
-export async function GET(request: NextRequest){
-    const id =  request.nextUrl.searchParams.get('id');
-    if(id) {
-       const itemById = await getItemById(id as string)
-        if(itemById){
-            return Response.json(toObject(itemById))
-        }
-    }
+export async function GET(request: NextRequest) {
+    try {
+        const id = request.nextUrl.searchParams.get('id');
+        if (id) {
+            const itemById = await getItemById(id as string)
+            if (itemById) {
+                return Response.json(toObject(itemById))
+            }
 
-    
-    const items = await prisma.item.findMany({
-        select:{
-          id: true,
-          code: true,
-          description: true,
-          quantity: true,
-          price: true,
-          total_price: true,
-          created_at: true,
-          updated_at: true
+            return Response.json({
+                success: false,
+                error: "No record found.",
+            }, { status: 404 });
         }
-    });
-    
-    const itemsFormat = items.map((item)=>toObject(item)) 
-    return Response.json({items: itemsFormat})
+
+        const items = await prisma.item.findMany({
+            select: {
+                id: true,
+                code: true,
+                description: true,
+                quantity: true,
+                price: true,
+                total_price: true,
+                created_at: true,
+                updated_at: true
+            }
+        });
+
+        const itemsFormat = items.map((item) => toObject(item))
+        return Response.json({ items: itemsFormat }, { status: 200 });
+    } catch (error: any) {
+        return Response.json({
+            error: error.message,
+        }, { status: 500 });
+    }
 }
 
 
-export async function POST(request: Request){
-    const item = await request.json();
-    await prisma.item.create({data:item});
-    return Response.json({
-         "success": true,
-     })
- }
- 
- 
- export async function PUT(request: NextRequest){
-    const id =  request.nextUrl.searchParams.get('id');
-    const item = await request.json();
+export async function PUT(request: NextRequest) {
+    try {
+        const id = request.nextUrl.searchParams.get('id');
+        const item = await request.json();
 
-   const editedItem = await prisma.item.update({
-        where:{
-            id: Number(id)
-        },
-        data:{
-            description: item.description,
-            quantity: item.quantity,
-            price: item.price,
-            total_price: item.quantity * item.price,
-            updated_at: new Date(),
+        const editedItem = await prisma.item.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                description: item.description,
+                quantity: item.quantity,
+                price: item.price,
+                total_price: item.quantity * item.price,
+                updated_at: new Date(),
+            }
+        });
+
+        return Response.json({ item: toObject(editedItem) }, { status: 201 })
+    } catch (error: any) {
+        return Response.json({
+            success: false,
+            error: error.message,
+        }, { status: 500 });
+    }
+}
+
+
+export async function DELETE(request: NextRequest) {
+    try {
+        const id = request.nextUrl.searchParams.get('id');
+
+        await prisma.item.delete({
+            where: {
+                id: Number(id)
+            }
+        });
+
+        return Response.json({ success: true }, { status: 200 })
+
+    } catch (error: any) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') {
+                return Response.json({
+                    error: "No records found to delete for this id.",
+                }, { status: 404 });
+            }
         }
-    });
-    
-    return Response.json({item: toObject(editedItem)})
- }
- 
-
- export async function DELETE(request: NextRequest){
-    const id =  request.nextUrl.searchParams.get('id');
-
-     await prisma.item.delete({
-        where:{
-            id: Number(id)
-        }
-    }); 
-    
-    return Response.json({
-         "success": true,
-     })
- }
+        return Response.json({
+            error: error.message,
+        }, { status: 500 });
+    }
+}
--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,7 +1,7 @@
 import csv from 'csv-parser';
 import prisma from '../../lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
-import { put, list, del  } from "@vercel/blob";
+import { put, list, del } from "@vercel/blob";
 import axios from 'axios';
 interface CsvData {
   code: string;
@@ -12,47 +12,48 @@ interface CsvData {
 }
 
 export async function POST(nextRequest: NextRequest) {
-      const formData  = await nextRequest.formData()
-      const file = formData.get("file");
+  try {
+    const formData = await nextRequest.formData()
+    const file = formData.get("file");
 
-      if (!file) {
-        return NextResponse.json(
-          { error: "File blob is required." },
-          { status: 400 }
-        );
-      }
+    if (!file) {
+      return NextResponse.json(
+        { error: "File blob is required." },
+        { status: 400 }
+      );
+    }
 
-      await put('uploads/data.csv', file, { access: 'public' });
+    await put('uploads/data.csv', file, { access: 'public' });
 
-      const downloadFile = (url: string) => {
-        return new Promise((resolver) => {
-          const results: CsvData[] = [];
-          axios({
-            url,
-            responseType: 'stream'
-          }).then((response: any) => {
-            const stream = response.data.pipe(csv())
+    const downloadFile = (url: string) => {
+      return new Promise((resolver) => {
+        const results: CsvData[] = [];
+        axios({
+          url,
+          responseType: 'stream'
+        }).then((response: any) => {
+          const stream = response.data.pipe(csv())
             .on('data', (data: CsvData) => results.push(data))
-            .on('end',async () => {
-                for (const result of results) {
+            .on('end', async () => {
+              for (const result of results) {
                 await prisma.item.create({
-                    data: {
-                      code: result.code,
-                      description: result.description,
-                      quantity: Number(result.quantity),
-                      price:  Number(result.price),
-                      total_price: Number(result.total_price),
-                      updated_at: new Date(),
-                      created_at: new Date(),
-                    }
-                  });
+                  data: {
+                    code: result.code,
+                    description: result.description,
+                    quantity: Number(result.quantity),
+                    price: Number(result.price),
+                    total_price: Number(result.total_price),
+                    updated_at: new Date(),
+                    created_at: new Date(),
+                  }
+                });
               }
             });
-            stream.on('finish', resolver);
-          });
+          stream.on('finish', resolver);
         });
-      };
-  
+      });
+    };
+
 
     const response = await list();
     await Promise.all(response.blobs.map(async (blob) => {
@@ -60,6 +61,12 @@ export async function POST(nextRequest: NextRequest) {
       await del(blob.url);
     }));
 
-    return Response.json({success: true});
-    
+    return Response.json({  success: true })
+
+  } catch (error: any) {
+    return Response.json({
+      success: false,
+      error: error.message,
+    }, { status: 500 });
+  }
 }
